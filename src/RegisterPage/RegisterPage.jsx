7c8b86daa613d6d9e545a5cb7f2d0307ab7a8cdf
michalkaniewski <michalkaniewski99@gmail.com>
2020-04-03 21:55:28 +0200

    first view of registration page

---
+++ src/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import { Container, Card, Form, Button } from "react-bootstrap";
+
+class RegisterPage extends React.Component {
+  state = {
+    email: "",
+    name: "",
+    username: "",
+    password: "",
+    passwordR: "",
+    areEqual: true,
+    validated: false,
+    redirect: false
+  };
+
+  onChange = (e, val) => {
+    const value = e.target.value;
+
+    this.setState({
+      [val]: value
+    });
+  };
+
+
+  handleSubmit = event => {
+    if(this.state.password !== this.state.passwordR){
+        this.setState({
+            areEqual: false
+        });
+    }
+
+    alert(this.state.name);
+  };
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    });
+  };
+
+  render() {
+    const {
+      email,
+      name,
+      username,
+      password,
+      passwordR,
+      areEqual,
+      validated
+    } = this.state;
+    const { onChange, handleSubmit } = this;
+    return (
+      <Container>
+            <Card>
+                <Card.Header as="h2">
+                    Rejestracja
+                </Card.Header>
+                <Card.Body>
+                    <Form
+                        noValidate
+                        validated={validated}
+                        onSubmit={handleSubmit}
+                    >
+                        <Form.Group controlId="formGroupFirstName" className="">
+                            <Form.Control
+                                type="text"
+                                autoComplete="on"
+                                placeholder="Name"
+                                value={name}
+                                onChange={e => onChange(e, "name")}
+                                required
+                                minLength="1"
+                                maxLength="30"
+                            />
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                required
+                                value={email}
+                                onChange={e => onChange(e, "email")}
+                                className="loginPage__input"
+                                maxLength="50"
+                            />
+                            <Form.Control
+                                type="text"
+                                placeholder="Nick"
+                                required
+                                value={username}
+                                onChange={e => onChange(e, "username")}
+                                maxLength="50"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formGroupPassword">
+                        <Form.Control
+                            type="password"
+                            autoComplete="on"
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => onChange(e, "password")}
+                            required
+                            minLength="6"
+                            maxLength="30"
+                        />
+
+                        <Form.Control.Feedback type="invalid">
+                            Minimum 6 characters
+                        </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="formGroupPasswordRepeat">
+                        <Form.Control
+                            type="password"
+                            placeholder="Powtórz hasło"
+                            value={passwordR}
+                            onChange={e => onChange(e, "passwordR")}
+                            required
+                            minLength="6"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Minimum 6 characters
+                        </Form.Control.Feedback>
+                        {!areEqual ? (
+                            <small className="invalidMessage">
+                            Passwors are different!
+                            </small>
+                        ) : (
+                            ""
+                        )}
+                        </Form.Group>
+                        <Button
+                            variant="secondary"
+                            className="loginPage__button"
+                            type="submit"
+                        >
+                            Create account
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </Container>
+    )};
+} 
+
+//RegisterPage.contextType = UserContext;
+
+export default RegisterPage;
