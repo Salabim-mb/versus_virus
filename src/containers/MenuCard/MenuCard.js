055171db09da750d49eb716b5476f94dfc8cccdb
balasm <01142551@pw.edu.pl>
2020-04-03 22:35:34 +0200

    basic working component

--- src/containers/MenuCard/MenuCard.js
+++ src/containers/MenuCard/MenuCard.js
@@ -2,18 +2,23 @@ import React from "react";
 import {Card, Container, Button} from 'react-bootstrap';
 import "./MenuCard.css";
 
-const MenuCard = ({title, body, hide}) => (
-    <Container className="MenuCard_container">
-        <Card className="MenuCard_card">
-            <Card.Header as="h2" className="MenuCard_header">
-                {title}
-                <Button variant="danger" className="MenuCard_button_close" onClick={e=> hide(e)}>X</Button>
-            </Card.Header>
-            <Card.Body className="MenuCard_body">
-                {body}
-            </Card.Body>
-        </Card>
-    </Container>
-);
+class MenuCard extends React.Component {
+  render() {
+      const {title, body, hide} = this.props;
+      return (
+          <Container className="MenuCard_container">
+              <Card className="MenuCard_card">
+                  <Card.Header as="h2" className="MenuCard_header">
+                      {title}
+                      <Button variant="danger" className="MenuCard_button_close" onClick={e=> hide(e)}>X</Button>
+                  </Card.Header>
+                  <Card.Body className="MenuCard_body">
+                      {body}
+                  </Card.Body>
+              </Card>
+          </Container>
+      );
+  }
+};
 
 export default MenuCard;
