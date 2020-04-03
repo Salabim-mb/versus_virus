5920efc4b6516a7c730751a67410b78b87a93e9c
balasm <01142551@pw.edu.pl>
2020-04-03 22:37:47 +0200

    removed unused component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import MenuCard from "./containers/MenuCard/MenuCard";
 import "./Styles/global.css"
 
 const App = () => (
     <div className="App">
-      <MenuCard title="Hello world" body="Hi!" hide={null} />
+      <p> Hello world! </p>
     </div>
 );
 
