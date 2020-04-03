e61083258db9049053c6970b17b9257ca58f4ef6
balasm <01142551@pw.edu.pl>
2020-04-03 23:19:48 +0200

    Router update

--- src/containers/MenuCard/MenuCard.js
+++ src/containers/MenuCard/MenuCard.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {Card, Container, Button} from 'react-bootstrap';
+import {Card, Container} from 'react-bootstrap';
 import "./MenuCard.css";
+
+// const BrowserHistory = require('react-router/lib/BrowserHistory').default;
 
 class MenuCard extends React.Component {
   render() {
-      const {title, body, hide} = this.props;
+      const {title, children} = this.props;
       return (
           <Container className="MenuCard_container">
               <Card className="MenuCard_card">
                   <Card.Header as="h2" className="MenuCard_header">
                       {title}
-                      <Button variant="danger" className="MenuCard_button_close" onClick={e=> hide(e)}>X</Button>
+                      {/*<Button variant="danger" className="MenuCard_button_close" onClick={BrowserHistory.goBack}>X</Button>*/}
                   </Card.Header>
                   <Card.Body className="MenuCard_body">
-                      {body}
+                      {children}
                   </Card.Body>
               </Card>
           </Container>
