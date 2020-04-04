de8d36d2c10d1c0f98c253983b480a280ed634c1
balasm <01142551@pw.edu.pl>
2020-04-04 12:23:27 +0200

    added context to app

--- src/root/App.js
+++ src/root/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import "../Styles/global.css";
+import {UserProvider} from "../context/UserContext";
 
 const App = () => (
-    <div className="App">
-      <Router>
-        <Route path="/" component={null}/>
-      </Router>
-    </div>
+    <UserProvider>
+        <div className="App">
+          <Router>
+            <Route path="/" component={null}/>
+          </Router>
+        </div>
+    </UserProvider>
 );
 
 export default App;
