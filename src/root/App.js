f41ed043a6f148338d0cf52729579ca4d964c35d
balasm <01142551@pw.edu.pl>
2020-04-04 12:07:41 +0200

    reorganised directories

--- src/root/App.js
+++ src/root/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import "./Styles/global.css";
+import "../Styles/global.css";
 
 const App = () => (
     <div className="App">
